52d4b17d773e2aeec5d1bddd999cd30926d52192
Pip Taylor <git@evilgeek.co.uk>
2016-05-23 17:03:37 +0100

    Tidy up initializer.
    Use a neater method for performing the SQL, and use start_with? for
    matching the version string.

--- lib/enforce_mysql_version/railtie.rb
+++ lib/enforce_mysql_version/railtie.rb
@@ -1,9 +1,9 @@
 module EnforceMysqlVersion
   class Railtie < Rails::Railtie
     initializer "enforce_mysql_version" do
-      mysql_version = ActiveRecord::Base.connection.execute("select version()").first.first
+      mysql_version = ActiveRecord::Base.connection.select_value("select version()")
 
-      unless mysql_version =~ /^#{EnforceMysqlVersion::REQUIRED_VERSION}/
+      unless mysql_version.start_with?(EnforceMysqlVersion::REQUIRED_VERSION)
         puts "App startup was prevented by the gem enforce_mysql_version. This app won't start unless the MySQL major/minor version is #{EnforceMysqlVersion::REQUIRED_VERSION}"
         exit(false)
       end
